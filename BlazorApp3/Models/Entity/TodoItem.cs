8ec1362185e53735783849fa7c85a75ae59f57dc
buyab_h9pfnco <buyab_h9pfnco@MOSHOPC>
2024-04-30 19:08:04 +0300

    Add project files.

---
+++ BlazorApp3/Models/Entity/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorApp3.Models.Entity
+{
+    public class TodoItem
+
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime? DueDate { get; set; }
+
+
+    }
+}
---
+++ BlazorApp3/Models/TodoDbcontext.cs
@@ -0,0 +1,21 @@
+﻿using BlazorApp3.Models.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorApp3.Models
+{
+    public class TodoDbcontext : DbContext
+    {
+
+
+        public DbSet<TodoItem> todoItems { get; set; }
+
+        public TodoDbcontext(DbContextOptions<TodoDbcontext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+
+    }
+
+}
